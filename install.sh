531a168832dbd88b084d67271e00a6903a342935
Eric Savadian <eric.j.savadian@gmail.com>
2015-07-26 04:53:52 -0400

    Backup existing config, make safer and assume less

--- install.sh
+++ install.sh
@@ -1,22 +1,50 @@
 #!/bin/bash
 # Install VIM configuration for local account
 
-if [ $(pwd) == $HOME ]
+# If tmux is installed, install .tmux.conf and backup existing one if present
+if [ -z ${TMUX+x} ]
 then
-  # download Vundle
-  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-  # build directories
-  mkdir .vim/colors
-
-  # move theme to appropriate folder
-  mv ./CandyPaper.vim ./.vim/colors/
-
-  # install Vundle plugins
-  vim +PluginInstall +qall
-
-  echo "All done!"
+  echo "tmux not installed -- skipping .tmux.conf install"
 else
-  echo "This script and the files contained in the package must be run out of your $HOME directory"       
+  if [ -f $HOME/.tmux.conf ]
+  then
+    echo ".tmux.conf file present -- moving to .tmux.conf.old"
+    mv $HOME/.tmux.conf $HOME/.tmux.conf.old
+
+    echo "...installing .tmux.conf"
+    mv tmux.conf $HOME/.tmux.conf
+  else
+    echo "tmux is present -- installing .tmux.conf"
+    mv tmux.conf $HOME/.tmux.con
+  fi  
 fi
+
+if [ -d $HOME/.vim ]
+then
+  echo "$HOME/.vim exists -- moving to $HOME/.vim.old"
+  mv $HOME/.vim $HOME/.vim.old
+fi
+
+if [ -f $HOME/.vimrc ]
+then
+  echo "$HOME/.vimrc exists -- moving to $HOME/.vimrc.old"
+  mv $HOME/.vimrc $HOME/.vimrc.old
+fi
+
+# install .vimrc
+mv ./.vimrc $HOME/.vimrc
+
+# download Vundle
+git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
+# build additioanl directories
+mkdir $HOME/.vim/colors
+
+# move theme to appropriate folder
+mv ./CandyPaper.vim $HOME/.vim/colors/
+
+# install Vundle plugins
+vim +PluginInstall +qall
+
+echo "All done!"
 
