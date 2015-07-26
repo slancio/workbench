c91840efe57170bf5f8c185a4a8df2974a8f2ae8
Eric Savadian <eric.j.savadian@gmail.com>
2015-07-26 06:51:49 -0400

    ellipses removed for clarity

--- install.sh
+++ install.sh
@@ -103,7 +103,7 @@ then
   # log directory tree of backed up files
   printf "\nContents of ./backups/${WORKBENCHBACKUP}\n" >> $WORKBENCHLOG
   ls -al ./backups/$WORKBENCHBACKUP/ >> $WORKBENCHLOG
-  printf "\nWrote out log to ${WORKBENCHLOG}...\n"
+  printf "\nWrote out log to ${WORKBENCHLOG}\n"
   printf "\n" >> $WORKBENCHLOG
 
   printf "All done!\n" | tee -a $WORKBENCHLOG
